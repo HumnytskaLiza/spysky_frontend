3b73753c216a2217a5d99381ed0191e3d73149de
malinkacat <gumnickajaliza@gmail.com>
2023-10-19 01:43:52 +0300

    update

---
+++ spysky-project/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue';
+import App from './src/App.vue';
+import VueRouter from 'vue-router';
+
+
+createApp(App).mount('#app')
+
--- spysky-project/src/App.vue
+++ spysky-project/src/App.vue
@@ -1,26 +1,24 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import { Scene, PerspectiveCamera, WebGLRenderer } from 'three';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  const scene = new Scene();
+  const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
+
+  scene.add(camera);
+
+  onMounted(() => {
+    const renderer = new WebGLRenderer({
+    canvas: document.querySelector('#canvas') as HTMLCanvasElement,
+    antialias: true,
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.render(scene, camera);
+  })
+
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+  <canvas id="canvas"></canvas>
+</template>
+
+
