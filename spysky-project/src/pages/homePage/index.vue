d5016437ade77eb4ece29f5803334ab083fec266
malinkacat <gumnickajaliza@gmail.com>
2023-11-19 22:40:32 +0200

    Finish fixing routes and scene load

--- spysky-project/src/pages/homePage/index.vue
+++ spysky-project/src/pages/homePage/index.vue
@@ -13,127 +13,205 @@
         </div>
     </div>
 </div>
-<div id="model-container-home"></div>
+<div id="model-container-home" ref="containerHome"></div>
 </template>
   
 <script setup>
-// import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.118/build/three.module.js";
-// import gsap from 'gsap';
-// import vertexShader from "../../assets/shaders-folder/vertex.glsl";
-// import fragmentShader from "../../assets/shaders-folder/fragment.glsl";
-// import atmosphereVertexShader from "../../assets/shaders-folder/atmosphereVertex.glsl";
-// import atmosphereFragmentShader from "../../assets/shaders-folder/atmosphereFragment.glsl";
+// Import Three.js library
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.118/build/three.module.js";
 
-// window.addEventListener('load', () => {
-//     // Define container
-//     const container = document.getElementById('model-container-home');
+// Import hooks
+import { onBeforeMount, onBeforeUnmount, onBeforeUpdate, onMounted, ref } from "vue";
 
-//     // Add style parameters to container
-//     container.style.width = "100vw";
-//     container.style.height = "100vh";
-//     container.style.position = "absolute";
-//     container.style.overflow = "hidden";
-//     container.style.top = "0";
-//     container.style.left = "0";
+// Import shaders
+import gsap from 'gsap';
+import vertexShader from "../../assets/shaders-folder/vertex.glsl";
+import fragmentShader from "../../assets/shaders-folder/fragment.glsl";
+import atmosphereVertexShader from "../../assets/shaders-folder/atmosphereVertex.glsl";
+import atmosphereFragmentShader from "../../assets/shaders-folder/atmosphereFragment.glsl";
 
-//     // Get width and height for a scene
-//     const w = container.offsetWidth;
-//     const h = container.offsetHeight;
+// Init the scene
+const scene = new THREE.Scene();
 
-//     // Add scene
-//     const scene = new THREE.Scene();
-//     const backgroundTexture = new THREE.TextureLoader().load('src/assets/background-textures/bg.png');
-//     scene.background = backgroundTexture;
+// Add the model-container to containerHome
+const containerHome = ref(null);
 
-//     // Add camera 
-//     const camera = new THREE.PerspectiveCamera(50, w / h, 0.1, 1000);
-//     camera.position.z = 12;
+// Init the camera
+const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 12;
 
-//     // Add renderer
-//     const renderer = new THREE.WebGLRenderer({
-//         antialias: true
-//     });
-//     renderer.setSize(w, h);
-//     renderer.setPixelRatio(window.devicePixelRatio);
-//     container.appendChild(renderer.domElement);
+// Init the renderer
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 
-//     // Create an Earth texture
-//     const material = new THREE.ShaderMaterial({
-//         vertexShader,
-//         fragmentShader,
-//         uniforms: {
-//             globeTexture: {
-//                 value: new THREE.TextureLoader().load('src/assets/textures/daymap.jpg')
-//             }
-//         }
-//     });
+// Add background to the scene
+const backgroundTexture = new THREE.TextureLoader().load('src/assets/background-textures/bg.png');
+scene.background = backgroundTexture;
 
-//     // Add a sphere
-//     const sphere = new THREE.Mesh(new THREE.SphereGeometry(5, 50, 50), material)
+// --- Creating the Earth ---
 
-//     // Create atmosphere material
-//     const material_atmosphere = new THREE.ShaderMaterial({
-//         vertexShader: atmosphereVertexShader,
-//         fragmentShader: atmosphereFragmentShader,
-//         blending: THREE.AdditiveBlending,
-//         side: THREE.BackSide
-//     });
+// Init an Earth material
+const material = new THREE.ShaderMaterial({
+      vertexShader,
+      fragmentShader,
+      uniforms: {
+          globeTexture: {
+              value: new THREE.TextureLoader().load('src/assets/textures/daymap.jpg')
+          }
+      }
+  });
 
-//     // Add the atmosphere
-//     const atmosphere = new THREE.Mesh(new THREE.SphereGeometry(5, 50, 50), material_atmosphere)
-//     atmosphere.scale.set(1.05, 1.05, 1.05);
-    
-//     // Group the sphere and the atmosphere
-//     const group = new THREE.Group();
-//     group.add(sphere);  
-//     group.add(atmosphere); 
-//     scene.add(group);
+const geometry =  new THREE.SphereGeometry(7, 50, 50);
+  
+// Init the Earth
+const sphere = new THREE.Mesh(geometry, material)
 
-//     group.position.x = 6;
-//     group.position.y = -2;
+// --- Creating the atmosphere
 
-//     // Create mouse obj
-//     const mouse = {
-//         x: 0,
-//         y: 0
-//     };
+// Init the atmosphere material
+const material_atmosphere = new THREE.ShaderMaterial({
+        vertexShader: atmosphereVertexShader,
+        fragmentShader: atmosphereFragmentShader,
+        blending: THREE.AdditiveBlending,
+        side: THREE.BackSide
+    });
 
-//     // Animation
-//     function animate() {
-//         requestAnimationFrame(animate);
-//         renderer.render(scene, camera);
-//         sphere.rotation.y += 0.002;
-//         group.rotation.y = mouse.x * 0.5;
-//         gsap.to(group.rotation, {
-//             x: -mouse.y * 0.5,
-//             y: mouse.x * 0.5,
-//             duration: 2
-//         })
-//     }
-//     animate();
+// Init the atmosphere
+const atmosphere = new THREE.Mesh(geometry, material_atmosphere)
+atmosphere.scale.set(1.05, 1.05, 1.05);
 
-//     // Get mouse coords
-//     addEventListener('mousemove', (event) => {
-//         mouse.x = (event.clientX / innerWidth) * 2 - 1
-//         mouse.y = -(event.clientY / innerHeight) * 2 - 1
-//     })
+// Init a group for models
+const group = new THREE.Group();
+    group.add(sphere);  
+    group.add(atmosphere);     
+    group.position.x = 6;
+    group.position.y = -2;
 
-//     // Handle size of scene
-//     function handleWindowResize () {
-//         const newWidth = window.innerWidth;
-//         const newHeight = window.innerHeight;
+// Create mouse obj
+const mouse = {
+        x: 0,
+        y: 0
+};
 
-//         container.style.width = `${newWidth}px`;
-//         container.style.height = `${newHeight}px`;
-//         renderer.setSize(newWidth, newHeight);
+// Handling window size
+function handleWindowResize () {
+    const newWidth = window.innerWidth;
+    const newHeight = window.innerHeight;
 
-//         camera.aspect = newWidth / newHeight;
-//         camera.updateProjectionMatrix();
-//     }
+    // Check if model-container exists in containerHome
+    if (containerHome.value) {
+        containerHome.value.style.width = `${newWidth}px`;
+        containerHome.value.style.height = `${newHeight}px`;
+        renderer.setSize(newWidth, newHeight);
+        camera.aspect = newWidth / newHeight;
+    }
 
-//     window.addEventListener('resize', handleWindowResize, false);
-//     handleWindowResize();
-// });
+    camera.updateProjectionMatrix();
+}
+
+// Call the hook before the component is mounted
+onBeforeMount(() => {
+  window.addEventListener('resize', handleWindowResize);
+
+  // Get mouse coords
+  addEventListener('mousemove', (event) => {
+        mouse.x = (event.clientX / innerWidth) * 2 - 1
+        mouse.y = -(event.clientY / innerHeight) * 2 - 1
+    })
+
+  handleWindowResize();
+})
+
+// Call the hook when the component is mounted
+onMounted(() => {
+    // Add container to the ref
+    containerHome.value = document.getElementById('model-container-home');
+    console.log(containerHome.value);
+
+    // Add style parameters to container
+    containerHome.value.style.width = "100vw";
+    containerHome.value.style.height = "100vh";
+    containerHome.value.style.position = "absolute";
+    containerHome.value.style.overflow = "hidden";
+    containerHome.value.style.top = "0";
+    containerHome.value.style.left = "0";
+
+    // Get width and height for a scene
+    const w = containerHome.value.offsetWidth;
+    const h = containerHome.value.offsetHeight;
+
+    camera.aspect = w/h
+
+    // Add objects to the scene
+    scene.add(group);
+
+    renderer.setSize(w, h);
+    renderer.setPixelRatio(window.devicePixelRatio);
+
+    containerHome.value.appendChild(renderer.domElement);
+
+    // Animation
+    function animate() {
+        requestAnimationFrame(animate);
+        renderer.render(scene, camera);
+        sphere.rotation.y += 0.002;
+        group.rotation.y = mouse.x * 0.5;
+        gsap.to(group.rotation, {
+            x: -mouse.y * 0.5,
+            y: mouse.x * 0.5,
+            duration: 2
+        })
+    }
+    animate();
+
+    window.addEventListener('resize', handleWindowResize);
+});
+
+// Call the hook to clear the scene
+onBeforeUnmount(() => {  
+  // Remove all scene objects
+  if (sphere) {
+    if (sphere.material) {
+      sphere.material.dispose();
+    }
+    if (sphere.geometry) {
+      sphere.geometry.dispose();
+    }
+  }
+
+  if (atmosphere) {
+    if (atmosphere.material) {
+      atmosphere.material.dispose();
+    }
+    if (atmosphere.geometry) {
+      atmosphere.geometry.dispose();
+    }
+  }
+
+  scene.remove(group);
+  
+  if (material) {
+    material.dispose();
+  }
+  if (material_atmosphere) {
+    material_atmosphere.dispose();
+  }
+  if (geometry) {
+    geometry.dispose();
+  }
+  if (scene.background) {
+    scene.background.dispose();
+  }
+  if (camera) {
+    scene.remove(camera);
+  }
+
+  renderer.dispose();
+  scene.dispose();
+  
+  containerHome.value.removeChild(renderer.domElement);
+  window.removeEventListener('resize', handleWindowResize);
+});
+
 </script>
   
 <style>
--- spysky-project/src/pages/loginPage/index.vue
+++ spysky-project/src/pages/loginPage/index.vue
@@ -1,10 +1,41 @@
 <template>
+  <div id="overlay">
+    <router-link to="/">
+        <button id="back-button" class="button">Return back</button>
+    </router-link>
+    <div id="form-container">
+      <div id="log-header">Log in</div>
+      <form>
+        <div>
+          <input type="text" id="email" name="email" placeholder="Email"><br>
+        </div>
+        <div>
+          <input type="password" id="password" name="password" placeholder="Password"><br>
+          <div id="link-container"><a href="#" id="forgot-password">Forgot password?</a></div>
+        </div>
+        <input type="submit" value="Log in" class="button" id="login-button">
+        <div id="socials">
+          <button class="button" id="google">
+            <img src="../../assets/icons/google.svg" alt="google">
+          </button>
+          <button class="button" id="facebook">
+            <img src="../../assets/icons/facebook.svg" alt="facebook">
+          </button>
+          <button class="button" id="apple">
+            <img src="../../assets/icons/apple.svg" alt="apple">
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
   <div id="model-container" ref="containerLog"></div>
 </template>
 
 <script setup>
 // Import Three.js library
 import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.118/build/three.module.js";
+
+// Import hooks
 import { onBeforeMount, onBeforeUnmount, onBeforeUpdate, onMounted, ref } from "vue";
 
 // Import shaders
@@ -21,14 +52,14 @@ const containerLog = ref(null);
 
 // Init the camera
 const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 20;
+camera.position.z = 10;
 
 // Init the renderer
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 
 // Add background to the scene
-// const backgroundTexture = new THREE.TextureLoader().load('src/assets/background-textures/bg.png');
-// scene.background = backgroundTexture;
+const backgroundTexture = new THREE.TextureLoader().load('src/assets/background-textures/bg.png');
+scene.background = backgroundTexture;
 
 // --- Creating the Earth ---
 
@@ -46,7 +77,27 @@ const material = new THREE.ShaderMaterial({
 const geometry =  new THREE.SphereGeometry(7, 50, 50);
   
 // Init the Earth
-const sphere1 = new THREE.Mesh(geometry, material)
+const sphere = new THREE.Mesh(geometry, material)
+
+// --- Creating the atmosphere
+
+// Init the atmosphere material
+const material_atmosphere = new THREE.ShaderMaterial({
+        vertexShader: atmosphereVertexShader,
+        fragmentShader: atmosphereFragmentShader,
+        blending: THREE.AdditiveBlending,
+        side: THREE.BackSide
+    });
+
+// Init the atmosphere
+const atmosphere = new THREE.Mesh(geometry, material_atmosphere)
+atmosphere.scale.set(1.05, 1.05, 1.05);
+
+// Init a group for models
+const group = new THREE.Group();
+    group.add(sphere);  
+    group.add(atmosphere);     
+    group.position.y = -7;
 
 // Handling window size
 function handleWindowResize () {
@@ -64,18 +115,17 @@ function handleWindowResize () {
     camera.updateProjectionMatrix();
 }
 
+// Call the hook before the component is mounted
 onBeforeMount(() => {
-  console.log("onBeforeMount was called")
-  
-
   window.addEventListener('resize', handleWindowResize);
   handleWindowResize();
 })
 
+// Call the hook when the component is mounted
 onMounted(() => {
-  console.log("onMounted was called")
-
+  // Add container to the ref
   containerLog.value = document.getElementById('model-container');
+
   // Add style parameters to container
   containerLog.value.style.width = "100vw";
   containerLog.value.style.height = "100vh";
@@ -84,17 +134,14 @@ onMounted(() => {
   containerLog.value.style.top = "0";
   containerLog.value.style.left = "0";
 
-
   // Get width and height for a scene
   const w = containerLog.value.offsetWidth;
   const h = containerLog.value.offsetHeight;
-  console.log(w, h)
-
-  console.log(camera.aspect)
 
   camera.aspect = w/h
 
-  scene.add(sphere1);
+  // Add objects to the scene
+  scene.add(group);
 
   renderer.setSize(w, h);
   renderer.setPixelRatio(window.devicePixelRatio);
@@ -104,98 +151,177 @@ onMounted(() => {
   function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
-    sphere1.rotation.y += 0.002;
+    sphere.rotation.y += 0.002;
   }
   animate();
 
   window.addEventListener('resize', handleWindowResize);
-  console.log(sphere1);
 });
 
-    
-
+// Call the hook to clear the scene
 onBeforeUnmount(() => {  
-  if (sphere1) {
-    if (sphere1.material) {
-      sphere1.material.dispose();
+  // Remove all scene objects
+  if (sphere) {
+    if (sphere.material) {
+      sphere.material.dispose();
     }
-
-    if (sphere1.geometry) {
-      sphere1.geometry.dispose();
+    if (sphere.geometry) {
+      sphere.geometry.dispose();
     }
-
-    scene.remove(sphere1);
   }
 
-  sphere1 = null;
+  if (atmosphere) {
+    if (atmosphere.material) {
+      atmosphere.material.dispose();
+    }
+    if (atmosphere.geometry) {
+      atmosphere.geometry.dispose();
+    }
+  }
+
+  scene.remove(group);
   
-    if (material) {
-      material.dispose();
-    }
-    if (geometry) {
-      geometry.dispose();
-    }
+  if (material) {
+    material.dispose();
+  }
+  if (material_atmosphere) {
+    material_atmosphere.dispose();
+  }
+  if (geometry) {
+    geometry.dispose();
+  }
+  if (scene.background) {
+    scene.background.dispose();
+  }
+  if (camera) {
+    scene.remove(camera);
+  }
 
   renderer.dispose();
-  camera.dispose();
   scene.dispose();
   
   containerLog.value.removeChild(renderer.domElement);
   window.removeEventListener('resize', handleWindowResize);
-  console.log("onBeforeUnmount was called")
-  console.log(sphere1);
 });
 
-    
-
-    // Add scene
-    
-    // const backgroundTexture = new THREE.TextureLoader().load('src/assets/background-textures/bg.png');
-    // scene.background = backgroundTexture;
-
-    // Add camera 
-    //const camera = new THREE.PerspectiveCamera(50, w / h, 0.1, 1000);
-
-    // Add renderer
-    // const renderer = new THREE.WebGLRenderer({
-    //     antialias: true
-    // });
-    
-
-    // Create Earth texture
-    // const material = new THREE.ShaderMaterial({
-    //     vertexShader,
-    //     fragmentShader,
-    //     uniforms: {
-    //         globeTexture: {
-    //             value: new THREE.TextureLoader().load('src/assets/textures/daymap.jpg')
-    //         }
-    //     }
-    // });
-
-    // Add sphere
-
-    // // Create atmosphere material
-    // const material_atmosphere = new THREE.ShaderMaterial({
-    //     vertexShader: atmosphereVertexShader,
-    //     fragmentShader: atmosphereFragmentShader,
-    //     blending: THREE.AdditiveBlending,
-    //     side: THREE.BackSide
-    // });
-
-    // // Add atmosphere
-    // const atmosphere = new THREE.Mesh(new THREE.SphereGeometry(7, 50, 50), material_atmosphere)
-    // atmosphere.scale.set(1.05, 1.05, 1.05);
-    
-    // const group = new THREE.Group();
-    // group.add(sphere);  
-    // group.add(atmosphere); 
-    // scene.add(group);
-    
-    // group.position.y = -7;
-
-    
 </script>
 
 <style>
+#overlay {
+  z-index: 10000;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+}
+
+#overlay div > input {
+  background-color: transparent;
+  border: 0;
+  border-bottom: 2px solid #FFF;
+  width: 100%;
+  padding-bottom: 5px;
+  outline: none;
+}
+
+#overlay div > input::placeholder {
+  color: #FFF;
+}
+
+#back-button {
+  position: absolute;
+  top: 20px;
+  left: 20px;
+  z-index: 1000;
+}
+
+.button {
+  font-size: 15px;
+  font-family: 'Exo 2', sans-serif;
+  color: white;
+  padding: 10px 20px;
+  border: 0;
+  border-radius: 8px;
+  background: rgba(255, 121, 0, 0.82);
+  box-shadow: 0px 0px 10px 1px rgba(0, 0, 0, 0.17);
+  cursor: pointer;
+  transition: 0.5s;
+  width: fit-content;
+}
+
+#login-button {
+  margin-left: auto;
+  margin-right: auto;
+}
+
+#form-container {
+  position: relative;
+  z-index: 1000;
+  width: 55%;
+  height: 70%;
+  border-radius: 5px;
+  background: rgba(89, 96, 133, 0.50);
+  box-shadow: 0px 4px 16px 0px rgba(0, 0, 0, 0.17);
+  backdrop-filter: blur(8px);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 70px;
+}
+
+#form-container form {
+  display: flex;
+  flex-direction: column;
+  gap: 45px;
+}
+
+#socials {
+  display: flex;
+  flex-direction: row;
+  gap: 50px;
+}
+
+#socials img {
+  height: 40px;
+}
+
+#socials button,
+#login-button {
+  padding: 13px 60px;
+}
+
+#log-header {
+  color: #FFF;
+  font-family: 'Exo 2', sans-serif;
+  font-size: 64px;
+  font-style: normal;
+  font-weight: 500;
+}
+
+#form-container form input,
+#form-container form label {
+  color: #FFF;
+  font-family: 'Exo 2', sans-serif;
+  font-size: 30px;
+  font-style: normal;
+  font-weight: 400;
+}
+
+#link-container {
+  width: 100%;
+  text-align: end;
+  margin-top: 20px;
+}
+
+#forgot-password {
+  color: #FFF;
+  font-family: 'Exo 2', sans-serif;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 400;
+  text-decoration: none;
+}
 </style>
