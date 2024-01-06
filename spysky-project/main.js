878e551c5af51bca8105c81bbfb1eba21e1f3907
malinkacat <gumnickajaliza@gmail.com>
2024-01-07 01:56:56 +0200

    Cleaned branch

---
+++ spysky-project/main.js
@@ -0,0 +1,44 @@
+// Import Application
+import { createApp } from 'vue'
+import App from './src/App.vue'
+
+// Import vue-router
+import { createRouter, createWebHistory } from 'vue-router';
+
+// Import components
+import login from './src/pages/loginPage/index.vue';
+import signup from './src/pages/signupPage/index.vue';
+import welcome from './src/pages/welcomePage/index.vue';
+import home from './src/pages/homePage/index.vue';
+
+// Define routes
+const routes = [
+  {
+    path: '/login',
+    component: login, 
+  },
+  {
+    path: '/signup',
+    component: signup, 
+  },
+  {
+    path: '/',
+    component: welcome, 
+  },
+  {
+    path: '/home',
+    component: home,
+  },
+];
+
+// Create router
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+// Create app
+const app = createApp(App);
+
+app.use(router);
+app.mount('#app');
